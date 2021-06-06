c9394e230f53035de50285e581c65b026d036d44
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-06-06 14:41:31 +0200

    Got everything to run and build with bazel.

--- code/unix/init.cpp
+++ code/unix/init.cpp
@@ -1,8 +1,8 @@
 #include "command_line.h"
 #include "time.h"
 
-void Com_Init(char *s) {}
-void NET_Init() {}
+extern "C" void Com_Init(char*);
+extern "C" void NET_Init();
 
 void InitializeGame(const int argc, const char *argv[]) {
   InitTimeBase();
