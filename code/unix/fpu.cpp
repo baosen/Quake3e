021257f95c56a057708ef1343a1039afe38b0df3
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-06-12 21:38:21 +0200

    Simplified code.

--- code/unix/fpu.cpp
+++ code/unix/fpu.cpp
@@ -1,18 +1,13 @@
-void Sys_ConfigureFPU() // bk001213 - divide by zero
-{
-#ifdef __linux__
-#ifdef __i386
-#ifdef __GLIBC__
+void Sys_ConfigureFPU() {
+#if defined(__linux__) && defined(__i386) && defined(__GLIBC__)
 #ifndef NDEBUG
   // Enable FPE's in debug mode.
   static int fpu_word = _FPU_DEFAULT & ~(_FPU_MASK_ZM | _FPU_MASK_IM);
   int current = 0;
   _FPU_GETCW(current);
-#else  // NDEBUG
+#else
   static int fpu_word = _FPU_DEFAULT;
   _FPU_SETCW(fpu_word);
-#endif // NDEBUG
-#endif // __GLIBC__
-#endif // __i386
-#endif // __linux
+#endif
+#endif
 }
