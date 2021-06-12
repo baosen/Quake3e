db4bfcdf8b42b087ee33c1e8a13142a60bcd772f
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-06-12 21:42:43 +0200

    Simplified code.

--- code/unix/loop.cpp
+++ code/unix/loop.cpp
@@ -1,7 +1,7 @@
 #include "fpu.h"
 
 extern "C" void IN_Frame();
-extern "C" void Com_Frame(const bool noDelay);
+extern "C" void Com_Frame(const bool);
 extern "C" bool CL_NoDelay();
 
 void DoGameLoop() {
