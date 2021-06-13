f8a0f1745260795cd4022bf322903f23e3d8f1fb
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-06-13 11:54:49 +0200

    Use external SDL.h.

--- code/sdl/sdl_input.c
+++ code/sdl/sdl_input.c
@@ -20,14 +20,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-#ifdef USE_LOCAL_HEADERS
-#	include "SDL.h"
-#else
-#	include <SDL.h>
-#endif
-
 #include "../client/client.h"
 #include "sdl_glw.h"
+#include <SDL.h>
 
 static cvar_t *in_keyboardDebug;
 
